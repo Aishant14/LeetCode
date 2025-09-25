1a9296fadd0ce7f8bab3c2703ee0d524e29ba2d7
Aishant14 <119166803+Aishant14@users.noreply.github.com>
2025-09-25 21:04:38 +0530

    Time: 27 ms (92.12%) | Memory: 39.7 MB (85.16%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,15 +1,39 @@
+class node{
+        public:
+        int val;
+        int pos;
+
+        node(int val, int pos){
+            this->val = val;
+            this->pos = pos;
+        }
+};
+struct mycomp{
+    bool operator()(node&n1, node&n2){
+        return n1.val > n2.val;
+    }
+};
+
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        set<int> st(arr.begin(),arr.end());
-        unordered_map<int,int> mp;
-        int rank =1;
-        for(auto it: st){
-            mp[it] = rank++;
+        priority_queue<node,vector<node>,mycomp> pq;
+        for(int i =0; i<arr.size(); i++){
+            node n1(arr[i],i);
+            pq.push(n1);
         }
-        vector<int> res;
-        for(int i = 0; i < arr.size(); i++) {
-            arr[i] = mp[arr[i]];
+        int rank = 0;
+        int prev = INT_MIN;
+        while(!pq.empty()){
+            node curr = pq.top();
+            if(prev != curr.val){
+                arr[curr.pos] = ++rank;
+                prev = curr.val;
+            }
+            else{
+                arr[curr.pos] = rank;
+            }
+            pq.pop();
         }
         return arr;
     }
