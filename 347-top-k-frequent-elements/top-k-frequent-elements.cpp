1786971d0200d2f9cb796383657f0ba4dd4d9562
Aishant14 <119166803+Aishant14@users.noreply.github.com>
2025-10-04 20:29:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (59.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-       unordered_map<int,int> mp1;
-       int n = nums.size();  
-       for(int num:nums){   // for pair number with their frequancy
-            mp1[num]++;
-       }
-
-       vector<vector<int>> bucket (n+1); // creating bucket to hold elements
-       for(auto pair : mp1){           //transfers elements to vector based on their frequancy;
-            int element = pair.first;
+        unordered_map<int,int> mp;
+        for(int num: nums){
+            mp[num]++;
+        }
+        priority_queue<pair<int,int>> pq;
+        for(auto pair : mp){
+            int val = pair.first;
             int freq = pair.second;
-            bucket[freq].push_back(element);
+            pq.push({freq,val});
         }
-        vector <int> finalarr;                //vector will will hold the most freq element based on k
-        for(int i=n; i >=0 && k > finalarr.size(); i--){
-            for(int ele : bucket[i])
-            {
-                finalarr.push_back(ele);
-                if(finalarr.size() == k) break;
-            }
+        vector<int> res;
+        while(k> 0){
+            auto top = pq.top();
+            pq.pop();
+            int val =top.second;
+            res.push_back(val);
+            k--;
         }
-        return finalarr;
-
-
-
+        return res;
     }
 };
