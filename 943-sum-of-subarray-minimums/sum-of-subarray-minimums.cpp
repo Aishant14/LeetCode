055d3f5e9f0008c948bf1e33383d11ede16dc856
Aishant14 <119166803+Aishant14@users.noreply.github.com>
2025-11-13 21:02:24 +0530

    Time: 16 ms (61.27%) | Memory: 50.2 MB (6.80%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,54 +1,47 @@
 class Solution {
 public:
-    vector<int> getNSL(vector<int> arr, int n) {
+    vector<int> togetNSL(vector<int> arr, int n){
+        stack<int> st;
         vector<int> res(n);
-        stack<int> leftst;
-        for (int i = 0; i < n; i++) {
-
-            while (!leftst.empty() && arr[i] < arr[leftst.top()]) {
-                leftst.pop();
+        //Right min
+        for(int i =0; i<n; i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
             }
-            if (leftst.empty()) {
-                res[i] = -1;
-            } else {
-                res[i] = leftst.top();
-            }
-            leftst.push(i);
+            if(st.empty()) res[i] = -1;
+            else res[i] = st.top();
+            st.push(i);
         }
         return res;
     }
- vector<int> getNSR(vector<int> arr, int n) {
-    vector<int> res(n);
-    stack<int> rightst;
-    for (int i = n - 1; i >= 0; i--) {
-
-        while (!rightst.empty() && arr[i] <= arr[rightst.top()]) {
-            rightst.pop();
+    vector<int> togetNSR(vector<int> arr, int n){
+        stack<int> st;
+        vector<int> res(n);
+        //Right min
+        for(int i = n-1; i>= 0; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            if(st.empty()) res[i] = n;
+            else res[i] = st.top();
+            st.push(i);
         }
-        if (rightst.empty()) {
-            res[i] = n;
-        } else {
-            res[i] = rightst.top();
+        return res;
+    }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        int M = 1e9 +7;
+        vector<int>NSL = togetNSL(arr,n);
+        vector<int>NSR = togetNSR(arr,n);
+        long long sum = 0;
+        for(int i=0; i<n; i++){
+            // to find the number of sub array that have arr[i] as lowest number 
+            long long ls = i - NSL[i];
+            long long rs = NSR[i] - i;
+            long long totalWays = ls*rs;
+            long long totalsum =  totalWays*arr[i];
+            sum = (sum + totalsum)%M; 
         }
-    rightst.push(i);
+        return (int)sum;
     }
-    return res;
-}
-
-int sumSubarrayMins(vector<int>& arr) {
-    int n = arr.size();
-    int M = 1e9 + 7;
-    vector<int> NSL = getNSL(arr, n);
-    vector<int> NSR = getNSR(arr, n);
-    long long sum = 0;
-    for (int i = 0; i < n; i++) {
-        long long ls = i - NSR[i];
-        long long rs =  NSL[i] -i;
-        long long totalWays = ls * rs;
-        long long totalsum = totalWays * arr[i];
-        sum = (sum + totalsum) % M;
-    }
-    return (int)sum;
-}
-}
-;
+};
